c4553b83a8637e2315856b78fd35b5d67fed868e
root <root@centos6.vagrant.internal>
2019-11-06 14:40:32 +0000

    first time commit in this repo

---
+++ src/main/java/w3s1126/App.java
@@ -0,0 +1,13 @@
+package w3s1126;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
